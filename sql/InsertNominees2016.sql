e5675782bcc6f31a1f5c61bc6e65193cd1cf1945
Frank Kearney <fkearney8@gmail.com>
2017-02-25 17:45:20 -0800

    Saving the insert SQL for the 2017 nominees.

---
+++ sql/InsertNominees2016.sql
@@ -0,0 +1,146 @@
+insert into nominees values (DEFAULT, 1, 'Arrival');
+insert into nominees values (DEFAULT, 1, 'Fences');
+insert into nominees values (DEFAULT, 1, 'Hacksaw Ridge');
+insert into nominees values (DEFAULT, 1, 'Hell or High Water');
+insert into nominees values (DEFAULT, 1, 'Hidden Figures');
+insert into nominees values (DEFAULT, 1, 'La La Land');
+insert into nominees values (DEFAULT, 1, 'Lion');
+insert into nominees values (DEFAULT, 1, 'Manchester by the Sea');
+insert into nominees values (DEFAULT, 1, 'Moonlight');
+
+insert into nominees values (DEFAULT, 2, 'Arrival - Denis Villeneuve');
+insert into nominees values (DEFAULT, 2, 'Hacksaw Ridge - Mel Gibson');
+insert into nominees values (DEFAULT, 2, 'La La Land - Damien Chazelle');
+insert into nominees values (DEFAULT, 2, 'Manchester by the Sea - Kenneth Lonergan');
+insert into nominees values (DEFAULT, 2, 'Moonlight - Barry Jenkins');
+
+insert into nominees values (DEFAULT, 3, 'Casey Affleck (Manchester by the Sea)');
+insert into nominees values (DEFAULT, 3, 'Andrew Garfield (Hacksaw Ridge)');
+insert into nominees values (DEFAULT, 3, 'Ryan Gosling (La La Land)');
+insert into nominees values (DEFAULT, 3, 'Viggo Mortensen (Captain Fantastic)');
+insert into nominees values (DEFAULT, 3, 'Denzel Washington (Fences)');
+
+insert into nominees values (DEFAULT, 4, 'Isabelle Huppert (Elle)');
+insert into nominees values (DEFAULT, 4, 'Ruth Negga (Loving)');
+insert into nominees values (DEFAULT, 4, 'Natalie Portman (Jackie)');
+insert into nominees values (DEFAULT, 4, 'Emma Stone (La La Land)');
+insert into nominees values (DEFAULT, 4, 'Meryl Streep (Florence Foster Jenkins)');
+
+insert into nominees values (DEFAULT, 5, 'Mahershala Ali (Moonlight)');
+insert into nominees values (DEFAULT, 5, 'Jeff Bridges (Hell or High Water)');
+insert into nominees values (DEFAULT, 5, 'Lucas Hedges (Manchester by the Sea)');
+insert into nominees values (DEFAULT, 5, 'Dev Patel (Lion)');
+insert into nominees values (DEFAULT, 5, 'Michael Shannon (Nocturnal Animals)');
+
+insert into nominees values (DEFAULT, 6, 'Viola Davis (Fences)');
+insert into nominees values (DEFAULT, 6, 'Naomie Harris (Moonlight)');
+insert into nominees values (DEFAULT, 6, 'Nicole Kidman (Lion)');
+insert into nominees values (DEFAULT, 6, 'Octavia Spencer (Hidden Figures)');
+insert into nominees values (DEFAULT, 6, 'Michelle Williams (Manchester by the Sea)');
+
+insert into nominees values (DEFAULT, 9, 'Kubo and the Two Strings');
+insert into nominees values (DEFAULT, 9, 'Moana');
+insert into nominees values (DEFAULT, 9, 'My Life as a Zucchini');
+insert into nominees values (DEFAULT, 9, 'The Red Turtle');
+insert into nominees values (DEFAULT, 9, 'Zootopia');
+
+insert into nominees values (DEFAULT, 21, 'Arrival - Bradford Young');
+insert into nominees values (DEFAULT, 21, 'La La Land - Linus Sandgren');
+insert into nominees values (DEFAULT, 21, 'Lion - Greig Fraser');
+insert into nominees values (DEFAULT, 21, 'Moonlight - James Laxton');
+insert into nominees values (DEFAULT, 21, 'Silence - Rodrigo Prieto');
+
+insert into nominees values (DEFAULT, 23, 'Allied - Joanna Johnston');
+insert into nominees values (DEFAULT, 23, 'Fantastic Beasts and Where to Find Them - Colleen Atwood');
+insert into nominees values (DEFAULT, 23, 'Florence Foster Jenkins - Consolata Boyle');
+insert into nominees values (DEFAULT, 23, 'Jackie - Madeline Fontaine');
+insert into nominees values (DEFAULT, 23, 'La La Land - Mary Zophres');
+
+insert into nominees values (DEFAULT, 11, 'Fire at Sea');
+insert into nominees values (DEFAULT, 11, 'I Am Not Your Negro');
+insert into nominees values (DEFAULT, 11, 'Life, Animated');
+insert into nominees values (DEFAULT, 11, 'O.J.: Made in America');
+insert into nominees values (DEFAULT, 11, '13th');
+
+insert into nominees values (DEFAULT, 12, 'Extremis');
+insert into nominees values (DEFAULT, 12, '4.1 Miles');
+insert into nominees values (DEFAULT, 12, 'Joe\'s Violin');
+insert into nominees values (DEFAULT, 12, 'Watani: My Homeland');
+insert into nominees values (DEFAULT, 12, 'The White Helmets');
+
+insert into nominees values (DEFAULT, 24, 'Arrival');
+insert into nominees values (DEFAULT, 24, 'Hacksaw Ridge');
+insert into nominees values (DEFAULT, 24, 'Hell or High Water');
+insert into nominees values (DEFAULT, 24, 'La La Land');
+insert into nominees values (DEFAULT, 24, 'Moonlight');
+
+insert into nominees values (DEFAULT, 10, 'Land of Mine (Denmark)');
+insert into nominees values (DEFAULT, 10, 'A Man Called Ove (Sweden)');
+insert into nominees values (DEFAULT, 10, 'The Salesman (Iran)');
+insert into nominees values (DEFAULT, 10, 'Tanna (Australia)');
+insert into nominees values (DEFAULT, 10, 'Toni Erdmann (Germany)');
+
+insert into nominees values (DEFAULT, 22, 'A Man Called Ove');
+insert into nominees values (DEFAULT, 22, 'Star Trek Beyond');
+insert into nominees values (DEFAULT, 22, 'Suicide Squad');
+
+insert into nominees values (DEFAULT, 16, 'Jackie');
+insert into nominees values (DEFAULT, 16, 'La La Land');
+insert into nominees values (DEFAULT, 16, 'Lion');
+insert into nominees values (DEFAULT, 16, 'Moonlight');
+insert into nominees values (DEFAULT, 16, 'Passengers');
+
+insert into nominees values (DEFAULT, 17, 'Audition (The Fools Who Dream) - La La Land; Music by Justin Hurwitz; Lyric by Benj Pasek and Justin Paul');
+insert into nominees values (DEFAULT, 17, 'Can\'t Stop The Feeling - Trolls; Music and Lyric by Justin Timberlake, Max Martin and Karl Johan Schuster');
+insert into nominees values (DEFAULT, 17, 'City of Stars - La La Land; Music by Justin Hurwitz; Lyric by Benj Pasek and Justin Paul');
+insert into nominees values (DEFAULT, 17, 'The Empty Chair - Jim: The James Foley Story; Music and Lyric by J. Ralph and Sting');
+insert into nominees values (DEFAULT, 17, 'How Far I\'ll Go - from Moana; Music and Lyric by Lin-Manuel Miranda');
+
+insert into nominees values (DEFAULT, 20, 'Arrival');
+insert into nominees values (DEFAULT, 20, 'Fantastic Beasts and Where to Find Them');
+insert into nominees values (DEFAULT, 20, 'Hail, Caesar!');
+insert into nominees values (DEFAULT, 20, 'La La Land');
+insert into nominees values (DEFAULT, 20, 'Passengers');
+
+insert into nominees values (DEFAULT, 15, 'Blind Vaysha');
+insert into nominees values (DEFAULT, 15, 'Borrowed Time');
+insert into nominees values (DEFAULT, 15, 'Pear Cider and Cigarettes');
+insert into nominees values (DEFAULT, 15, 'Pearl');
+insert into nominees values (DEFAULT, 15, 'Piper');
+
+insert into nominees values (DEFAULT, 13, 'Ennemis Intérieurs');
+insert into nominees values (DEFAULT, 13, 'La Femme et le TGV');
+insert into nominees values (DEFAULT, 13, 'Silent Nights');
+insert into nominees values (DEFAULT, 13, 'Sing');
+insert into nominees values (DEFAULT, 13, 'Timecode');
+
+insert into nominees values (DEFAULT, 18, 'Arrival');
+insert into nominees values (DEFAULT, 18, 'Deepwater Horizon');
+insert into nominees values (DEFAULT, 18, 'Hacksaw Ridge');
+insert into nominees values (DEFAULT, 18, 'La La Land');
+insert into nominees values (DEFAULT, 18, 'Sully');
+
+insert into nominees values (DEFAULT, 19, 'Arrival');
+insert into nominees values (DEFAULT, 19, 'Hacksaw Ridge');
+insert into nominees values (DEFAULT, 19, 'La La Land');
+insert into nominees values (DEFAULT, 19, 'Rogue One: A Star Wars Story');
+insert into nominees values (DEFAULT, 19, '13 Hours: The Secret Soldiers of Benghazi');
+
+insert into nominees values (DEFAULT, 25, 'Deepwater Horizon');
+insert into nominees values (DEFAULT, 25, 'Doctor Strange');
+insert into nominees values (DEFAULT, 25, 'The Jungle Book');
+insert into nominees values (DEFAULT, 25, 'Kubo and the Two Strings');
+insert into nominees values (DEFAULT, 25, 'Rogue One: A Star Wars Story');
+
+insert into nominees values (DEFAULT, 8, 'Arrival');
+insert into nominees values (DEFAULT, 8, 'Fences');
+insert into nominees values (DEFAULT, 8, 'Hidden Figures');
+insert into nominees values (DEFAULT, 8, 'Lion');
+insert into nominees values (DEFAULT, 8, 'Moonlight');
+
+insert into nominees values (DEFAULT, 7, 'Hell or High Water');
+insert into nominees values (DEFAULT, 7, 'La La Land');
+insert into nominees values (DEFAULT, 7, 'The Lobster');
+insert into nominees values (DEFAULT, 7, 'Manchester by the Sea');
+insert into nominees values (DEFAULT, 7, '20th Century Women');
+
