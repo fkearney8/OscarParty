2d51fd5559bf70c566cd6cc76e8c1adef936a722
Frank Kearney <fkearney8@gmail.com>
2018-02-25 01:36:03 -0800

    Stored next category cleanup.

--- src/main/java/com/oscarparty/data/NextCategory.scala
+++ src/main/java/com/oscarparty/data/NextCategory.scala
@@ -7,15 +7,4 @@ import com.oscarparty.data.nominees.CategoryName
   */
 object NextCategory {
   var storedNextCategory : CategoryName.Value = CategoryName.BestPicture
-
-  def nextCategory: CategoryName.Value = storedNextCategory
-
-//  def setNextCategory(nextCatName: CategoryName.Value) {
-//    val categoriesWithoutWinners = OscarNomineesDAO.categoriesWithoutWinners
-//    if (categoriesWithoutWinners.exists(_.id == nextCatName)) {
-//      storedNextCategory = nextCatName
-//    } else {
-//      storedNextCategory = categoriesWithoutWinners.headOption.fold(-1)(_.id)
-//    }
-//  }
 }
